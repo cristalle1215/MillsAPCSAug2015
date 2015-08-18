b28ab4878c9237b900a18851b8d2bbcc178125cd
cristalle1215 <cristallekwok@gmail.com>
2015-08-18 09:21:32 -0700

    August 18, 2015 Hello world demonstration
    feeling good

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * First program; this will do a simple println. Our first Java code
+ * 
+ * Cristalle Kwok
+ * August 18,2015
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello World! From Cristalle.");
+    }
+
+}
