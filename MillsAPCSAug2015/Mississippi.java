978f179c6ede51bf7349424cdd8f5a1a7eb6a80b
cristalle1215 <cristallekwok2gmail.com>
2015-08-25 22:34:08 -0700

    hw day 9
    Misssippi

---
+++ MillsAPCSAug2015/Mississippi.java
@@ -0,0 +1,63 @@
+
+/**
+ * HW Day 9 Mississippi.
+ * 
+ * Cristalle Kwok 
+ * 8/25/15
+ */
+public class Mississippi
+{
+    public static void LetterM(){
+        System.out.println("M     M");
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("MM M MM");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+    }
+    
+    public static void LetterI(){
+        System.out.println("IIIII");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("IIIII");
+    }
+    
+    public static void LetterS(){
+        System.out.println(" SSSSS ");
+        System.out.println("S     S");
+        System.out.println("S      ");
+        System.out.println(" SSSSS ");
+        System.out.println("      S");
+        System.out.println("S     S");
+        System.out.println(" SSSSS ");
+    }
+    
+    public static void LetterP(){
+        System.out.println("PPPPPP ");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+    }
+    
+    public static void main(String[] args){
+        LetterM();
+        LetterI();
+        LetterS();
+        LetterS();
+        LetterI();
+        LetterS();
+        LetterS();
+        LetterI();
+        LetterP();
+        LetterP();
+        LetterI();
+    }
+}
